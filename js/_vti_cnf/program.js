8312cd76fb78a46f2031a676bc556376cec6d3d6
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2014-11-12 14:13:56 -0500

    New files

---
+++ js/_vti_cnf/program.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|12 Nov 2014 19:01:46 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|ALEXS\\TzL
+vti_modifiedby:SR|ALEXS\\TzL
+vti_timecreated:TR|28 Oct 2014 10:39:53 -0000
+vti_nexttolasttimemodified:TW|12 Nov 2014 19:01:20 -0000
+vti_backlinkinfo:VX|AGT/index.html BetaAO/index.html AutoObsessionsGame/index.html
+vti_cacheddtm:TX|12 Nov 2014 19:01:46 -0000
+vti_filesize:IR|26541
