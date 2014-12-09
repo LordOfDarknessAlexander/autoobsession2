a05568fb176d47e55466c654628df2accb04f480
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2014-12-09 10:58:55 -0500

    check

---
+++ slots/js/_vti_cnf/globals.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|ALEXS\\TzL
+vti_modifiedby:SR|ALEXS\\TzL
+vti_timelastmodified:TR|09 Dec 2014 08:20:07 -0000
+vti_timecreated:TR|22 Nov 2014 20:21:36 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Bridge/index.html
+vti_nexttolasttimemodified:TW|09 Dec 2014 07:53:51 -0000
+vti_cacheddtm:TX|09 Dec 2014 08:20:07 -0000
+vti_filesize:IR|3487
---
+++ slots/js/_vti_cnf/jqueryLib.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|09 Dec 2014 07:36:01 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|BetaAO/index.html
+vti_author:SR|ALEXS\\TzL
+vti_modifiedby:SR|ALEXS\\TzL
+vti_timecreated:TR|26 Nov 2014 17:54:06 -0000
+vti_nexttolasttimemodified:TW|09 Dec 2014 07:34:25 -0000
+vti_cacheddtm:TX|09 Dec 2014 07:34:25 -0000
+vti_filesize:IR|1146
