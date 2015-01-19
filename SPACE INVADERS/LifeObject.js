72f0e96f8c5dddf7fdc8066586284e3b555dc02d
Andrew Best <andrew.c.best@gmail.com>
2015-01-19 16:32:20 -0500

    Obligatory update after progress
    None of it is hooked in, but it's the beginning of the lives.

---
+++ SPACE INVADERS/LifeObject.js
@@ -0,0 +1,20 @@
+var LifeObjectClass =
+{
+	xPos : 0,
+	yPos : 0,
+	width : 0,
+	height : 0,
+	isDrawn : true,		//true = is drawn on the canvas
+
+	init : function()
+	{
+	},
+
+	LifeObject.start = function()
+	{
+		this.xPos = 0;
+		this.yPos = 0;
+		this.width = 20;
+		this.height = 20;
+	}
+};
