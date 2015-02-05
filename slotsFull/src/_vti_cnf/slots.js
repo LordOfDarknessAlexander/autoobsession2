9c69714738faab34984a62f9d9d0c2c209e3fa9e
JimCorrigan <cyginus@gmail.com>
2015-02-05 16:06:14 -0500

    Mini version of slots game is started
    Thursday update as well

---
+++ slotsFull/src/_vti_cnf/slots.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|05 Feb 2015 18:28:38 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|JIMCORRIGAN\\Jim
+vti_modifiedby:SR|JIMCORRIGAN\\Jim
+vti_timecreated:TR|27 Jan 2015 14:12:04 -0000
+vti_backlinkinfo:VX|AutoObsessions/slots/game.html
+vti_nexttolasttimemodified:TW|05 Feb 2015 18:27:53 -0000
+vti_cacheddtm:TX|05 Feb 2015 18:28:38 -0000
+vti_filesize:IR|12072
