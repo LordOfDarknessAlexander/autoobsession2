bbf5d917089e53ddcb61884e94a293f28da4b3cb
Andrew Best <andrew.c.best@gmail.com>
2015-02-12 15:30:56 -0500

    Work on Space Shooter
    Player Lives, Score and Health all function as they are supposed to.

--- Space Game/Assets/Scripts/UI/UIControl.cs
+++ Space Game/Assets/Scripts/UI/UIControl.cs
@@ -6,6 +6,28 @@ public class UIControl : MonoBehaviour
 {
     public Button m_Start;
     public Button m_Quit;
+    public GameObject m_Player; //The player game object
+    public Slider m_PlayerHealth; //Slider for the player's current health
+    public Text m_LivesText; //Text element for the current number of lives the player has
+    public Text m_ScoreText; //Text element for the player's current score
+
+    private int maxPlayerHealth_; //Player's max health
+    private int minHealth_ = 0; //Minimum health the player or enemy can ever have, obviously it's 0
+
+    void Start()
+    {
+        maxPlayerHealth_ = m_Player.GetComponent<PlayerController>().m_Ship.m_HP;
+        m_PlayerHealth.maxValue = maxPlayerHealth_;
+        m_PlayerHealth.minValue = minHealth_;
+    }
+
+    void Update()
+    {
+        m_PlayerHealth.value = m_Player.GetComponent<PlayerController>().m_Ship.m_HP;
+        m_PlayerHealth.minValue = minHealth_;
+        m_LivesText.text = m_Player.GetComponent<PlayerController>().m_Lives.ToString();
+        m_ScoreText.text = m_Player.GetComponent<PlayerController>().m_Score.ToString();
+    }
 
    public void StartGame()
     {
