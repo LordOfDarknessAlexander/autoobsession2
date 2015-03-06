e7c0580703b46f265613e9c08517120173a84113
JimCorrigan <cyginus@gmail.com>
2015-03-06 13:10:33 -0500

    mid day update

---
+++ SpaceGame2.0/Assets/Scripts/EnemyObjectPool.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyObjectPool : MonoBehaviour 
+{
+    public static EnemyObjectPool m_Current;
+    //public GameObject m_PooledObject;
+    public GameObject[] m_PooledShips;
+    public int m_PooledAmt;
+
+    List<GameObject> PooledObjects;
+
+
+    void Awake()
+    {
+        m_Current = this;
+    }
+
+    void Start()
+    {
+        PooledObjects = new List<GameObject>();
+        foreach(GameObject shipObj in m_PooledShips)
+        {
+            for(int i = 0; i < m_PooledAmt; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(m_PooledShips[i]);
+                obj.SetActive(false);
+                PooledObjects.Add(obj);
+            }
+        }
+
+        
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for(int i = 0; i < PooledObjects.Count; i++)
+        {
+            if(!PooledObjects[i].activeInHierarchy)
+            {
+                return PooledObjects[i];
+            }
+        }
+        return null;
+    }
+}
---
+++ SpaceGame2.0/Assets/Scripts/Game/Player/SpawnPlayer.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnPlayer : MonoBehaviour 
+{
+    public GameController m_Control;
+    public GameObject m_Player;
+
+    public int m_PooledAmt = 3;
+    List<GameObject> lives_ = new List<GameObject>();
+
+    void Start()
+    {
+        
+    }
+
+    public void Spawn()
+    {
+        for (int i = 0; i < m_PooledAmt; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(m_Player);
+            obj.SetActive(false);
+            lives_.Add(obj);
+        }
+
+        m_Player = lives_[0];
+
+        m_Control.m_Lives = m_PooledAmt;
+
+       //m_Player = lives_[0];
+
+        if (m_Control.m_Lives > 0)
+        {
+            m_Control.m_LivesText.text = m_Control.m_Lives.ToString();
+
+            Vector3 playerSpawn_ = new Vector3(0.0f, -5.0f, 0.0f);
+            Quaternion spawnPlayerRotation = Quaternion.identity;
+
+            m_Player.SetActive(true);
+            m_Player.transform.position = playerSpawn_;
+            m_Player.transform.rotation = spawnPlayerRotation;
+            
+        }
+        else if (m_Control.m_Lives == 0)
+        {
+            m_Control.gameOver_ = true;
+            m_Control.GameOver();
+        } 
+    }
+
+    public void Respawn()
+    {
+        Spawn();
+    }
+        
+}
