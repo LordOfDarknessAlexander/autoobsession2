d29c6581e7da4c28070232ebc4f8ddaca79a7ef3
JimCorrigan <cyginus@gmail.com>
2015-03-10 13:41:43 -0400

    re loading Fluffy

---
+++ Fluffy Unicorn/Assets/Scripts/Controllers/UIController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour 
+{
+    public Button m_Start;
+    public Button m_Quit;
+
+   public void StartGame()
+    {
+        Application.LoadLevel("Main");
+    }
+
+    public void Quit()
+    {
+		
+    }
+}
