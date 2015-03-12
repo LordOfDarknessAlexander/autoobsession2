6febfc02e41c7a3fdc228bceef802c8b963bf150
Andrew Best <andrew.c.best@gmail.com>
2015-03-12 16:21:17 -0400

    Upgrade UI complete

--- SpaceGame3.0/Assets/Scripts/UI/StarMapUI.cs
+++ SpaceGame3.0/Assets/Scripts/UI/StarMapUI.cs
@@ -4,45 +4,37 @@ using UnityEngine.UI;
 
 public class StarMapUI : MonoBehaviour
 {
-    public Button Level1;
-    public Button Level2;
-    public Button Level3;
-    public Button Level4;
-    public Button Level5;
-    public Button SpaceStation;
+    public GameObject m_UpgradePanel; //Upgrade panel UI element
+    public Text m_HealthText; //Text UI element for the health cost and upgrade level
+    public Text m_ShieldText; //Text UI element for the shield cost and upgrade level
+    public Text m_DamageText; //Text UI element for the damage cost and upgrade level
+    public Text m_EngineText; //Text UI element for the engine cost and upgrade level
 
-    public int m_CurrLevel;
+    //--------TEMP CODE----------
+    public GameObject m_TestObj; //Test object
 
-    public enum Levels
+    void Update()
     {
-        LEVEL1,
-        LEVEL2,
-        LEVEL3,
-        LEVEL4,
-        LEVEL5
-    }
-    public Levels m_Level = Levels.LEVEL1;
-
-
-    public void SetLevelData()
-    {
-        switch (m_Level)
-        {
-            case Levels.LEVEL1:
-                break;
-            case Levels.LEVEL2:
-                break;
-            case Levels.LEVEL3:
-                break;
-            case Levels.LEVEL4:
-                break;
-            case Levels.LEVEL5:
-                break;
-        };
+        m_HealthText.text = "Cost: " + m_TestObj.GetComponent<PlayerShip>().HealthUpgradeCost.ToString() + "\nCurrent Level: " + m_TestObj.GetComponent<PlayerShip>().HealthLevel;
+        m_ShieldText.text = "Cost: " + m_TestObj.GetComponent<PlayerShip>().ShieldUpgradeCost.ToString() + "\nCurrent Level: " + m_TestObj.GetComponent<PlayerShip>().ShieldLevel;
+        m_DamageText.text = "Cost: " + m_TestObj.GetComponent<PlayerShip>().DamageUpgradeCost.ToString() + "\nCurrent Level: " + m_TestObj.GetComponent<PlayerShip>().DamageLevel;
+        m_EngineText.text = "Cost: " + m_TestObj.GetComponent<PlayerShip>().EngineUpgradeCost.ToString() + "\nCurrent Level: " + m_TestObj.GetComponent<PlayerShip>().EngineLevel;
     }
 
     public void SetLevel()
     {
         Camera.main.GetComponent<LevelData>().m_CurrLevel = 0;
+    }
+
+    //Button function -- If the user presses the spaceship this function will fire
+    //                -- The function opens up the Upgrade menu, where they can then upgrade their ship or upgrade to a higher tier ship entirely
+    public void UpgradeMenu()
+    {
+        m_UpgradePanel.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        m_UpgradePanel.SetActive(false);
     }
 }
